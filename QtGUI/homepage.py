8aceb78fae6f2b5010b6169088ea1fd633b30b66
GridPWR <dmtsamson@gmail.com>
2025-03-08 23:12:27 +0900

    homepage.py and ui_homepage.py
    Update homepage.py and Generate ui_homepage.py using PyQt6

--- QtGUI/homepage.py
+++ QtGUI/homepage.py
@@ -1,1 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from ui_homepage import Ui_Form  # Import the UI class
 
+class HomePage(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.central_widget = QWidget()  # Create a central widget
+        self.ui = Ui_Form()  # Create an instance of the UI
+        self.ui.setupUi(self.central_widget)  # Set up the UI on central_widget
+        self.setCentralWidget(self.central_widget)  # Assign the central widget to QMainWindow
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = HomePage()
+    window.show()
+    app.exec()
